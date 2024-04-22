e727da6823775545011d291411b1c3104415ab0e
Anamika <anamikaparoo@gmail.com>
2024-04-22 23:32:34 +0530

    commit

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,17 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
+import { data } from '../utils/Data';
+import Search from './Search';
 function Main() {
+  const [resetdata,setresetdata]=useState("");
+  const dataval = data;
+  const store = dataval.map((ele)=>{return <Card val={ele}/>})
   return (
     <div class="flex justify-around flex-wrap mt-6 p-8"> 
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        {store}
     </div>
   )
 }
---
+++ src/utils/Data.js
@@ -0,0 +1,75 @@
+export const data = [
+    {
+        title:"Frontend Development",
+        name: "Anurag Singh ProCodrr",
+        price:"Unpaid",
+        url:"https://www.youtube.com/watch?v=fc2axTKm4ps&list=PLfEr2kn3s-br9ZFmejfLhAgMbGgbpdof8&ab_channel=AnuragSinghProCodrr",
+        img:"https://i.ytimg.com/vi/fc2axTKm4ps/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCqkNPUCiJiGWTjoQff_8Xt0UREmA",
+        lang:"Hinglish"
+    },
+    {
+        title:"Frontend Development",
+        name: "Apna College",
+        price:"Unpaid",
+        url:"https://www.youtube.com/watch?v=l1EssrLxt7E&list=PLfqMhTWNBTe3H6c9OGXb5_6wcc1Mca52n&ab_channel=ApnaCollege",
+        img:"https://i.ytimg.com/vi/l1EssrLxt7E/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBeHsL0IAwmp4nDP4YTVkzvG9Z2OA",
+        lang:"Hinglish"
+    },
+    {
+        title:"Frontend Development",
+        name: "Sheryians Coding School",
+        price:"unpaid",
+        url:"https://www.youtube.com/watch?v=4dprtEzunIk&list=PLbtI3_MArDOkNtOan8BQkG6P8wf6pNVz-&ab_channel=SheryiansCodingSchool",
+        img:"https://i.ytimg.com/vi/4dprtEzunIk/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLADTU-hp8xbMZI1NHrVblgI4knT8Q",
+        lang:"Hinglish"
+    },
+    {
+        title:"Frontend Development",
+        name: "FreeCodeCamp",
+        price:"unpaid",
+        url:"https://www.youtube.com/watch?v=MsnQ5uepIaE&pp=ygUYZnJvbnRlbmQgZGV2ZWxvcG1lbnQgbGVj",
+        img:"https://www.freecodecamp.org/news/content/images/size/w2000/2022/08/frontend.png",
+        lang:"English"
+    },
+    {
+        title:"Blockchain",
+        name: "FreeCodeCamp",
+        price:"unpaid",
+        url:"https://www.youtube.com/watch?v=gyMwXuJrbJQ&pp=ygUKQmxvY2tjaGFpbg%3D%3D",
+        img:"https://www.freecodecamp.org/news/content/images/size/w2000/2022/05/blockchain1.png",
+        lang:"English"
+    },
+    {
+        title:"Frontend Development",
+        name: "CodeHelp - by Babbar",
+        price:"unpaid",
+        url:"https://www.youtube.com/watch?v=Vi9bxu-M-ag&list=PLDzeHZWIZsTo0wSBcg4-NMIbC0L8evLrD&ab_channel=CodeHelp-byBabbar",
+        img:"https://i.ytimg.com/vi/Vi9bxu-M-ag/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAuRKGTBkNUOEglcihlb91LN6XYOQ",
+        lang:"Hinglish"
+    },
+    {
+        title:"React",
+        name: "KG Coding by Prashant Sir",
+        price:"unpaid",
+        url:"https://www.youtube.com/watch?v=eILUmCJhl64&ab_channel=KGCodingbyPrashantSir",
+        img:"https://i.ytimg.com/vi/eILUmCJhl64/hqdefault.jpg",
+        lang:"Hinglish"
+    },
+    {
+        title:"MERN",
+        name: "Thapa Technical",
+        price:"unpaid",
+        url:"https://www.youtube.com/watch?v=IymPq7ik29k&list=PLwGdqUZWnOp2Z3eFOgtOGvOWIk4e8Bsr_",
+        img:"https://i.ytimg.com/vi/IymPq7ik29k/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA5S0seS-NCcYu4FON2-K4i8HYmEw",
+        lang:"Hinglish"
+    },
+    {
+        title:"Blockchain",
+        name: "FreeCodeCamp",
+        price:"unpaid",
+        url:"https://www.youtube.com/watch?v=gyMwXuJrbJQ&pp=ygUKQmxvY2tjaGFpbg%3D%3D",
+        img:"https://www.freecodecamp.org/news/content/images/size/w2000/2022/05/blockchain1.png",
+        lang:"English"
+    }
+   
+]
