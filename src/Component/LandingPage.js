f8505fdb9ce7d67e9feed55a9ba1e00a3aaffa4a
Anamika <anamikaparoo@gmail.com>
2024-04-29 23:24:34 +0530

    commit

--- src/Component/LandingPage.js
+++ src/Component/LandingPage.js
@@ -43,43 +43,26 @@ function LandingPage() {
 
   return (
     <>
-      <div className="flex justify-around">
-        <div>
-          <p className="text-4xl font-bold ml-20 mt-40"> One Stop Solution For Confusing <br /> Tech Content</p>
-          <p className="ml-20 mt-2 text-lg ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-          <button className="bg-transparent hover:bg-blue-500 ml-20 mt-6 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-            Explore Now
-          </button>
-        </div>
-        <img className="mt-32" src="https://img.freepik.com/free-vector/futuristic-classroom-little-children-study-with-high-tech-equipment_335657-3308.jpg" alt="courses" />
-      </div>
-      {showChat ? (
-        <div className="fixed bottom-0 right-0 m-8 bg-white p-4 rounded shadow w-80 h-[80vh] overflow-scroll flex flex-col">
-          <FontAwesomeIcon icon={faTimes} className="absolute top-1 right-2 cursor-pointer text-gray-500" onClick={clearChat} />
-          <div className="flex-grow">
-            {question && (
-              <div className="bg-gray-200 text-gray-800 p-3 rounded-lg">
-                {question}
-              </div>
-            )}
-            {chatResponse && (
-              <div className="bg-blue-200 text-gray-800 rounded-lg m-2 p-2">
-                {chatResponse}
-              </div>
-            )}
-          </div>
-          <div className="flex items-center">
-            <input type="text" value={inputValue} onChange={handleInputChange} placeholder="Type your message..." className="border border-gray-400 p-2 rounded mr-2 text-left" />
-            <button onClick={sendMessage} className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
-              Send
-            </button>
-          </div>
-        </div>
-      ) : (
-        <div className="fixed bottom-0 right-0 m-8">
-          <FontAwesomeIcon icon={faComments} className="text-blue-500 cursor-pointer" size="3x" onClick={toggleChat} />
-        </div>
-      )}
+      <div className="flex flex-col lg:flex-row justify-center items-center lg:justify-around">
+  <div className="text-center lg:text-left lg:w-1/2">
+    <p className="text-4xl font-bold mt-10 lg:mt-0">One Stop Solution For Confusing <br /> Tech Content</p>
+    <p className="mt-2 text-lg">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+    <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded mt-4">
+      Explore Now
+    </button>
+  </div>
+  <img className="mt-10 lg:mt-0 max-w-full lg:max-w-none" src="https://img.freepik.com/free-vector/futuristic-classroom-little-children-study-with-high-tech-equipment_335657-3308.jpg" alt="courses" />
+</div>
+{showChat ? (
+  <div className="fixed bottom-0 right-0 m-8 bg-white p-4 rounded shadow w-full lg:w-80 h-[80vh] overflow-scroll flex flex-col lg:flex-row">
+    {/* Chat component */}
+  </div>
+) : (
+  <div className="fixed bottom-0 right-0 m-8">
+    <FontAwesomeIcon icon={faComments} className="text-blue-500 cursor-pointer" size="3x" onClick={toggleChat} />
+  </div>
+)}
+
     </>
   );
 }
